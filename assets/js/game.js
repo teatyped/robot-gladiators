2d17d724e1ef8f944ebf698c82a20ab731c6a736
Khai Truong <kminht@gmail.com>
2022-02-28 21:11:14 -0500

    created fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("this is an aleerst from scrip file");
+// get playerName 
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+
+// this creates a function named "fight"
+function fight(){
+    window.alert("The fight has begun!");
+}
+fight();
